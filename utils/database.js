8f4be368724a3f50c0f2cebad359c008710c694f
Trong Doan <trong.doan@smoothstack.com>
2024-08-10 01:34:10 -0700

    fix deprecation

--- utils/database.js
+++ utils/database.js
@@ -1,27 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 let isConnected = false;
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
 
-    if(isConnected){
+    if (isConnected) {
         console.log('MongoDB is Connected');
         return;
     }
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName : "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            dbName: 'share_prompt',
+            // No need for useNewUrlParser and useUnifiedTopology
+        });
 
-        })
         isConnected = true;
         console.log('MongoDB is Connected');
-        
     } catch (error) {
-        console.log(error);
-        
+        console.error('Error connecting to MongoDB:', error);
+        throw error; // Optionally rethrow error to handle it elsewhere
     }
-}
+};
